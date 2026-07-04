c0279c00fe48f85f4e20b9a3ab39fedc8c0f160f
thinagaran8 <thinagaran8@gmail.com>
2026-07-04 21:51:09 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,16 @@
-const CACHE_NAME = 'ty-kalyanam-v1';
-const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE_NAME = 'ty-kalyanam-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
